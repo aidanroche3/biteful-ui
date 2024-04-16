0c4a833b4e8cecb0957de49e06e4c8b68b2146db
Ethan Moskowitz <moskowitz.e@northeastern.edu>
2024-04-16 06:23:06 +0000

    Adding Reviews

--- pages/Read Reviews/jsobjects/utils/utils.js
+++ pages/Read Reviews/jsobjects/utils/utils.js
@@ -1,3 +1,13 @@
 export default {
-
+	getStars: (rating) => {
+		const fullStar = '★';
+    const emptyStar = '☆';
+    const numFullStars = Math.round(rating);
+    
+    let stars = fullStar.repeat(numFullStars);
+    
+    stars += emptyStar.repeat(5 - stars.length);
+    
+    return stars;
+	}
 }
